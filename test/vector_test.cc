a58cf51c70eabd426ddafc394adb6d7d6e0d62d4
wonderzyp <2671078523@qq.com>
2022-04-24 18:15:46 +0800

    添加class类型的push_back

--- test/vector_test.cc
+++ test/vector_test.cc
@@ -15,7 +15,7 @@ using namespace std;
 //   std::cout<<std::endl;
 // }
 
-TEST(TestPushback, testInt){
+TEST(TestPushback, Int){
   mySTL::vector<int> iv;
   iv.push_back(2);
   iv.push_back(10);
@@ -27,7 +27,7 @@ TEST(TestPushback, testInt){
   // printVector(iv);
 }
 
-TEST(TestPushback, testString){
+TEST(TestPushback, String){
   mySTL::vector<string> sv;
   sv.push_back("hi");
   sv.push_back("hello");
@@ -36,6 +36,65 @@ TEST(TestPushback, testString){
   EXPECT_EQ(sv[1], "hello");
   EXPECT_EQ(sv[2], "world");
   // printVector(sv);
+}
+
+
+
+class Vector3{
+public:
+  Vector3(){};
+  Vector3(float scalar)
+    :x(scalar), y(scalar), z(scalar){};
+
+  Vector3(float _x, float _y, float _z)
+    :x(_x), y(_y), z(_z){};
+
+  Vector3(const Vector3& other)
+    :x(other.x), y(other.y), z(other.z){
+      printf("Copy\n");
+  }
+
+  Vector3(const Vector3&& other)
+    :x(other.x), y(other.y), z(other.z){
+      printf("Move\n");
+  }
+
+  ~Vector3(){
+    printf("Destory\n");
+  }
+
+  Vector3& operator=(const Vector3& other){
+    printf("Copy assignment\n");
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    return *this;
+  }
+
+  Vector3& operator=(Vector3&& other){
+    printf("Move assignment\n");  
+    x = other.x;
+    y = other.y;
+    z = other.z;
+    return *this;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Vector3& vec){
+    os<<vec.x<<', '<<vec.y<<', '<<vec.z;
+    return os;
+  }
+
+private:
+  float x{};
+  float y{};
+  float z{};
+};
+
+TEST(TestPushback, Class){
+  mySTL::vector<Vector3> vec;
+  vec.push_back(Vector3());
+  vec.push_back(Vector3(1.0f));
+  vec.push_back(Vector3(1.0f, 2.0f, 3.0f));
 }
 
 
