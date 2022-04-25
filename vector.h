4315d2151ac8dcebce493a875582569ef1afb12c
wonderzyp <2671078523@qq.com>
2022-04-25 11:13:44 +0800

    write by myself

--- vector.h
+++ vector.h
@@ -2,57 +2,5 @@
 
 namespace mySTL{
 
-template <typename T>
-class vector{
-public:
-
-  vector(){
-    printf("mySTL::vector construct\n");
-    realloc(2);
-  }
-  
-  void push_back(const T& value){
-    if (m_size >= m_capacity) 
-      realloc(m_size<<1);
-    
-    m_Data[m_size++] = value;
-  }
-
-    void push_back(T&& value){
-    if (m_size >= m_capacity) 
-      realloc(m_size<<1);
-    
-    m_Data[m_size++] = std::move(value);
-  }
-
-  T& operator[](size_t index) {return m_Data[index];}
-  const T& operator[](size_t index) const {return m_Data[index];}
-
-  size_t size(){return m_size;}
-
-private:
-  void realloc(size_t newCapacity){
-    T* newBlock = new T[newCapacity];
-
-    if (newCapacity < m_size)
-      m_size = newCapacity;
-
-    for (int i=0; i<m_size; ++i){
-      newBlock[i] = std::move(m_Data[i]);
-    }
-    
-    delete[] m_Data;
-    m_Data = newBlock;
-    m_capacity = newCapacity;
-  }
-
-private:
-  T* m_Data=nullptr;
-
-  size_t m_size=0;
-  size_t m_capacity=0;
-};
-
-
 }// namespace mySTL
 
